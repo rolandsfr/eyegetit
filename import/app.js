9edd97764f6117170e1a723a9e696025c03d08cb
Ilja Lapkovskis <ilja.lapkovskis@gmail.com>
2023-06-03 21:35:58 +0300

    Add import

--- import/app.js
+++ import/app.js
@@ -1,6 +1,7 @@
 const mariadb = require('mariadb');
 const fs = require('fs');
 require('dotenv').config()
+const async = require('async')
 
 const DOMAIN = process.env.IMAGE_BASE_URL
 const pool = mariadb.createPool({
@@ -22,18 +23,31 @@ const getConnection = function(){
 
 const content = fs.readFileSync('./words.json', 'utf8')
 const collection = JSON.parse(content)
+
+const insertQueue = []
+
 Object.keys(collection).forEach((key) => {
     const word = key
     const category = collection[key][0]
     const imgName = key.charAt(0).toUpperCase() + key.slice(1)
-
     const url = `${DOMAIN}/${imgName}/${imgName}.jpg`
-    console.log(key, ":", category, ' -> ', url)
-    getConnection().then((conn) => {
-        conn.query('insert into images (word, category, url) values (?, ?, ?)', [word, category, url]).then((res) => {
-            console.log('OK', res)
-        }).catch((err) => {
-            console.error(err)
-        })
-    })
+
+    insertQueue.push({word: word, category: category, url: url});
 })
+
+async.eachSeries(insertQueue, function(item, callback) {
+    getConnection().then(function(connection){
+        connection.query(`INSERT INTO images (word, category, url) VALUES ('${item.word}', '${item.category}', '${item.url}')`).then(function(result){
+            console.log(result);
+            connection.release();
+            callback();
+        }).catch(function(error){
+            console.log(error);
+            connection.release();
+            callback();
+        });
+    }).catch(function(error){
+        console.log(error);
+        callback();
+    });
+}).then(() => process.exit(0));
