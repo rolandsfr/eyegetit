7e0a349071373579f20854a8d4b8008dbd9c0179
Ilja Lapkovskis <ilja.lapkovskis@gmail.com>
2023-06-03 13:26:09 +0300

    Add .gitignore. Add cors allow-all

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const inputRouter = require('./routes/input');
@@ -11,9 +12,10 @@ const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'jade');
 
 app.use(express.json());
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
