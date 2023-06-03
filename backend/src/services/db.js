553da1c775c3eb83b80ca310584932c738e52ede
Ilja Lapkovskis <ilja.lapkovskis@gmail.com>
2023-06-03 20:11:10 +0300

    Add DB and WIP import script

---
+++ backend/src/services/db.js
@@ -0,0 +1,23 @@
+// here we import the mariadb
+const mariadb = require('mariadb');
+
+// here we create a new connection pool
+const pool = mariadb.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+// here we are exposing the ability to creating new connections
+module.exports={
+    getConnection: function(){
+        return new Promise(function(resolve,reject){
+            pool.getConnection().then(function(connection){
+                resolve(connection);
+            }).catch(function(error){
+                reject(error);
+            });
+        });
+    }
+}
