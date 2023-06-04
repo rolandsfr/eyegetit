c8ff050feedff448328766cf99f2ba274aa1b5b0
rolandsfr <rolands.affaires@gmail.com>
2023-06-04 15:33:16 +0300

    add textarea submit button

--- frontend/app/components/Input/Input.tsx
+++ frontend/app/components/Input/Input.tsx
@@ -1,12 +1,39 @@
+import { useRef } from "react";
 import styled from "styled-components";
 
-const Textarea = styled.textarea`
+const Wrapper = styled.div`
   width: 100%;
   box-shadow: inset 3px 3px 12px 1px rgba(0, 0, 0, 0.1);
   border-radius: 10px;
   border: none;
-  padding: 0.7em 0em 0.7em 0.7em;
+  padding: 0.3em 0em 0.3em 2em;
   box-sizing: border-box;
+  position: relative;
+  margin-top: 1em;
+  padding-bottom: 1em;
+
+  button {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    background: #e1e1e1;
+    border-radius: 10px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 0.5em 1.5em 0.5em 1.5em;
+    right: 1em;
+    bottom: 1em;
+  }
+`;
+
+const Textarea = styled.textarea`
+  width: 100%;
+  border: none;
+  box-sizing: border-box;
+  outline: none;
+  background: none;
+  width: 85%;
 `;
 
 interface InputProps {
@@ -15,7 +42,32 @@ interface InputProps {
 }
 
 const Input: React.FC<InputProps> = ({ definedValue, onValueChanged }) => {
-  return <Textarea onChange={(e) => onValueChanged && definedValue != e.target.value ? onValueChanged(e.target.value) : {}} defaultValue={definedValue} />;
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const handleInputChange = () => {
+    const textarea = textareaRef.current;
+
+    if (textarea) {
+      textarea.style.height = "auto"; // Reset the height to calculate the actual scroll height
+      textarea.style.height = `${textarea.scrollHeight}px`; // Set the height to the scroll height
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Textarea
+        ref={textareaRef}
+        onChange={(e) => {
+          handleInputChange();
+          return onValueChanged && definedValue != e.target.value
+            ? onValueChanged(e.target.value)
+            : {};
+        }}
+        defaultValue={definedValue}
+      ></Textarea>
+      <button className="submit">Submit</button>
+    </Wrapper>
+  );
 };
 
 export default Input;
